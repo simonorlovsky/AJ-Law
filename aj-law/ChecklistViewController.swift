973b3f96625460a2377439f7081fb755bdd80565
simonorlovsky <simon.orlovsky@yahoo.com>
2015-07-23 19:13:34 -0700

    first commit

---
+++ aj-law/ChecklistViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ChecklistViewController.swift
+//  aj-law
+//
+//  Created by Simon Orlovsky on 7/23/15.
+//  Copyright (c) 2015 simonorlovsky. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet var tableView: UITableView!
+     var items: [String] = ["Employment Handbook", "Offer Letter"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as! UITableViewCell
+        cell.textLabel?.text = self.items[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        println("You selected cell #\(indexPath.row)!")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
